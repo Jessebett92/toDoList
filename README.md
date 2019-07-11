52b218cf604da48909a55761c96711af8af0d5cd
Jessebett92 <43708947+Jessebett92@users.noreply.github.com>
2019-07-11 13:37:47 -0600

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# toDoList
-Created with CodeSandbox
---
+++ src/index.js
@@ -0,0 +1,58 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import TodoItem from "./todoItem";
+
+import "./styles.css";
+
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      todo: "",
+      todos: []
+    };
+  }
+
+  handleChange = () => {
+    this.setState({
+      todo: event.target.value
+    });
+  };
+
+  renderTodos = () => {
+    return this.state.todos.map(todo => {
+      return <TodoItem title={todo} />;
+    });
+  };
+
+  handleSubmit = () => {
+    event.preventDefault();
+    this.setState({
+      todos: [...this.state.todos, this.state.todo],
+      todo: ""
+    });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <h1>ToDo List</h1>
+        <form onSubmit={this.handleSubmit} className="add-todo">
+          <input
+            type="text"
+            placeholder="Add ToDo"
+            value={this.state.todo}
+            onChange={this.handleChange}
+          />
+          <button type="submit">Add</button>
+        </form>
+        {this.renderTodos()}
+      </div>
+    );
+  }
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
